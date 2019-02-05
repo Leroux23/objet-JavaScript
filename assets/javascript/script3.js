ec6121fa219ede483b2f770a758c5881640d89da
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-05 16:26:40 +0100

    tableau + boucle.

--- assets/javascript/script3.js
+++ assets/javascript/script3.js
@@ -1,9 +1,41 @@
-var tab1 = ['Pommes de terre', 'camembert', 'pommes', 'bière'];
+var repositoire = new Array(1234, "Coucou");
+repositoire.push('truc');
+
+var tab1 = ['pommes de terre', 'camembert', 'pommes', 'bière'];
 var tab2 = ['salade', 10, true, Math.PI];
 
-var reponse = "<ul>";
-for(var i = 0 ; i < tab1.length; i++ ) {
+tab1.push('whisky');
+tab1.push(tab2);
+tab1.push(22);
+tab1.push("zzzzzzzzzzz");
+tab1.sort();
+tab1.reverse();
 
+var reponse = "<ul>";
+
+for (var i = 0 ;  i < tab1.length ; i++) {
+  reponse += "<li>";
+
+  if (Array.isArray(tab1[i])) {
+    let tab2 = tab1[i];
+    reponse += "<ul>";
+
+    for (var j = 0 ; j < tab2.length ; j++) {
+      reponse += "<li>";
+      reponse += tab2[j];
+      reponse += "</li>";
+    }
+    reponse += "</ul>";
+  }
+
+  else {
+    reponse += tab1[i];
+  }
+
+  reponse += "</li>";
 }
+
 reponse += "</ul>";
 //reponse = reponse + "</ul>";
+
+document.write(reponse);
