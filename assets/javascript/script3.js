2ff9296f54763ad857c97a6eee87739608fb04cb
Salomé LEROUX <leroux.salome@orange.fr>
2019-02-05 14:20:58 +0100

    Création tabelaux JavaScript.

---
+++ assets/javascript/script3.js
@@ -0,0 +1,9 @@
+var tab1 = ['Pommes de terre', 'camembert', 'pommes', 'bière'];
+var tab2 = ['salade', 10, true, Math.PI];
+
+var reponse = "<ul>";
+for(var i = 0 ; i < tab1.length; i++ ) {
+
+}
+reponse += "</ul>";
+//reponse = reponse + "</ul>";
